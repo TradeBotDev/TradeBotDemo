1d21610a91f08027b11bcdd2dff73ad52872856f
avatar4ik3 <crossfirelet@gmail.com>
2021-07-12 17:21:22 +0600

    Добавил заглушку для общения с формером.

--- TradeMarket/DataTransfering/Transferrer.cs
+++ TradeMarket/DataTransfering/Transferrer.cs
@@ -22,9 +22,17 @@ namespace TradeMarket.DataTransfering
 
     public class Transferrer
     {
-        public ObservableCollection<FullOrder> orders { get; }
+        private static Transferrer _transferrer = null;
+        public static Transferrer GetInstance()
+        {
+            if(_transferrer == null)
+            {
+                _transferrer = new Transferrer();
+            }
+            return _transferrer;
+        }
         public event OrdersAddedEventHandler ordersChanged;
-       
+
 
         private List<FullOrder> sampleOrders = new List<FullOrder>
         {
@@ -34,7 +42,14 @@ namespace TradeMarket.DataTransfering
                 Id = "1",
                 Quantity = 2,
                 Price = 16.2D,
-                RemovePrice = 15D
+                RemovePrice = 15D,
+                LastUpdateDate = DateTime.Now.AddDays(3),
+                RemoveDate = DateTime.Now.AddDays(4),
+                Signature = new TradeBot.Common.OrderSignature
+                {
+                    Status = TradeBot.Common.OrderStatus.Open,
+                    Type = TradeBot.Common.OrderType.Buy
+                }
             }
         };
 
@@ -44,9 +59,7 @@ namespace TradeMarket.DataTransfering
             foreach(var order in sampleOrders)
             {
                 await Task.Delay(random.Next(0,2000));
-                orders.Add(order);
                 ordersChanged(this, new OrdersAddedEventArgs(new List<FullOrder> { order }));
-                
             }
         }
         
