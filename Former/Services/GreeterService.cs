7c16b1a8785313e7ce9a64d31118b079d4737310
Евгений Михайлюков <mihaylyukov99@gmail.com>
2021-07-12 14:52:19 +0600

    Начал писать протофайл алгоритм_формер

--- Former/Services/GreeterService.cs
+++ Former/Services/GreeterService.cs
@@ -7,20 +7,12 @@ using System.Threading.Tasks;
 
 namespace Former
 {
-    public class GreeterService : Greeter.GreeterBase
+    public class Former : Former
     {
-        private readonly ILogger<GreeterService> _logger;
-        public GreeterService(ILogger<GreeterService> logger)
+        private readonly ILogger<Former> _logger;
+        public Former(ILogger<Former> logger)
         {
             _logger = logger;
-        }
-
-        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-        {
-            return Task.FromResult(new HelloReply
-            {
-                Message = "Hello " + request.Name
-            });
         }
     }
 }
