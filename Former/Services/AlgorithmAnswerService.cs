96025ad663c60ddbf7390f7b8d9eb3e37ea08326
Евгений Михайлюков <mihaylyukov99@gmail.com>
2021-07-12 17:32:42 +0600

    удалил всё

--- Former/Services/AlgorithmAnswerService.cs
+++ Former/Services/AlgorithmAnswerService.cs
@@ -1,38 +1,12 @@
 using Grpc.Core;
 using Grpc.Net.Client;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using TradeMarket;
 using TradeBot.Common;
+using TradeMarket.Former;
 
 namespace Former
 {
-    class BuyOrder 
-    {
-        string id;
-        Order simple_order_info;
-        DateTime RemoveDate;
-        double RemovePrice;
-    }
-    class Order 
-    {
-        double Quantity;
-        double Price;
-        OrderSignature Signature;
-        DateTime LastUpdateDate;
-    }
-    class OrderSignature
-    {
-        double quantity = 1;
-        double price = 2;
-        OrderSignature signature = 3;
-        google.protobuf.Timestamp last_update_date = 4;
-    }
-
-
     public class AlgorithmAnswerService : Algorithm.AlgorithmAnswerService.AlgorithmAnswerServiceClient
     {
         private readonly ILogger<AlgorithmAnswerService> _logger;
@@ -45,7 +19,7 @@ namespace Former
         {
             using var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var client = new OrderFormerService.OrderFormerServiceClient(channel);
-            var orderSignature = new OrderSignature
+            var orderSignature = new TradeBot.Common.OrderSignature
             {
                 Status = OrderStatus.Open,
                 Type = OrderType.Buy
@@ -58,7 +32,6 @@ namespace Former
 
             await foreach (var response in call.ResponseStream.ReadAllAsync())
             {
-                response.
             }
 
         }
