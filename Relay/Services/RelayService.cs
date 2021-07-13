5fdfc5ced651708eb362bac4320f368afb388a00
PC\deeplay <minebuilder445@gmail.com>
2021-07-13 09:43:52 +0600

    Добавил кода в файлик

---
+++ Relay/Services/RelayService.cs
@@ -0,0 +1,33 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TradeBot.Relay.Facade;
+using TradeBot.Common;
+
+namespace Relay
+{
+    class RelayService : SendToRelay.SendToRelayBase
+    {
+        private readonly ILogger<RelayService> _logger;
+        public RelayService(ILogger<RelayService> logger) => _logger = logger;
+
+        //public override Task<RelayService> StartBot(StartBotRequest request, ServerCallContext context)
+        //{
+        //    DefaultReply reply = new DefaultReply
+        //    {
+        //        Code = ReplyCode.Succeed,
+        //        Message = "Бот запущен успешно"
+        //    };
+
+        //    StartBotReply result = new StartBotReply
+        //    {
+        //        Reply = reply
+        //    };
+
+            
+        //}
+    }
+}
