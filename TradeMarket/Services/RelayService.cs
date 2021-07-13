c3da564cf55b00e46d65c3272fdc08b22fc521ed
avatar4ik3 <crossfirelet@gmail.com>
2021-07-13 16:22:45 +0600

    крассивы коммит

---
+++ TradeMarket/Services/RelayService.cs
@@ -0,0 +1,49 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TradeMarket.DataTransfering;
+using TradeMarket.Model;
+using TradeMarket.Relay;
+
+
+namespace TradeMarket.Services
+{
+    public class RelayService: OrderProviderService.OrderProviderServiceBase
+    {
+        private SubscriptionService<SubscribeOrdersRequest, SubscribeOrdersReply,FullOrder, RelayService> _subscriptionService;
+
+        private ILogger<RelayService> _logger;
+
+        private SubscribeOrdersReply Convert(FullOrder order)
+        {
+            return new SubscribeOrdersReply
+            {
+                SimpleOrderInfo = new TradeBot.Common.Order
+                {
+                    Price = order.Price,
+                    Quantity = order.Quantity,
+                    Signature = order.Signature,
+                    LastUpdateDate = new Timestamp()
+                    {
+                        Seconds = order.LastUpdateDate.Second
+                    }
+                }
+            };
+        }
+
+        public RelayService(FakeOrderSubscriber subscriber, ILogger<RelayService> logger)
+        {
+            _subscriptionService = new(subscriber,logger,Convert);
+            _logger = logger;
+        }
+
+        public async override Task SubscribeOrders(SubscribeOrdersRequest request, IServerStreamWriter<SubscribeOrdersReply> responseStream, ServerCallContext context)
+        {
+            await _subscriptionService.Subscribe(request, responseStream, context);
+        }
+    }
+}
---
+++ TradeMarket/Services/SubscriptionService.cs
@@ -0,0 +1,66 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using TradeMarket.DataTransfering;
+using TradeMarket.Model;
+
+namespace TradeMarket.Services
+{
+    /// <summary>
+    /// Класс который отвечает на ответы запросов по подписке .
+    /// </summary>
+    ///
+    //TODO LocalClass такое себе название !
+    public class SubscriptionService<SubscribeRequest, SubscribeReply,LocalClass, Service>
+        where SubscribeRequest : Google.Protobuf.IMessage<SubscribeRequest>
+        where SubscribeReply : Google.Protobuf.IMessage<SubscribeReply>
+    {
+        private Subscriber<LocalClass> _subscriber;
+
+        private ILogger<Service> _logger;
+
+        public static Converter<LocalClass, SubscribeReply> _converter;
+
+        public SubscriptionService(Subscriber<LocalClass> subscriber, ILogger<Service> logger, Converter<LocalClass, SubscribeReply> converter)
+        {
+            _logger = logger;
+            _subscriber = subscriber;
+            _converter = converter;
+
+        }
+
+        public async Task Subscribe(SubscribeRequest request, IServerStreamWriter<SubscribeReply> responseStream, ServerCallContext context)
+        {
+            _subscriber.Changed += async (sender, args) =>
+            {
+                await WriteStreamAsync(responseStream, _converter(args.Changed));
+            };
+            await AwaitCancellation(context.CancellationToken);
+
+        }
+
+        public async Task WriteStreamAsync(IServerStreamWriter<SubscribeReply> stream, SubscribeReply reply)
+        {
+            try
+            {
+                await stream.WriteAsync(reply);
+            }
+            catch (Exception exception)
+            {
+                //TODO что делать когда разорвется соеденение ?
+                _logger.LogWarning("Connection was interrupted by network servicies.");
+            }
+        }
+
+        private static Task AwaitCancellation(CancellationToken token)
+        {
+            var completion = new TaskCompletionSource<object>();
+            token.Register(() => completion.SetResult(null));
+            return completion.Task;
+        }
+    }
+}
