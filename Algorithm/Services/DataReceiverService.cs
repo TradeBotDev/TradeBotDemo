6fa4e21b62a0ac5c97f33b7f66c672f55fd7b342
Elena <dezvencio@gmail.com>
2021-07-14 16:06:21 +0600

    заглушка для получателя (хз)

--- Algorithm/Services/DataReceiverService.cs
+++ Algorithm/Services/DataReceiverService.cs
@@ -4,36 +4,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TradeBot.Algorithm.DataService.v1;
+using TradeBot.Common.v1;
+using System.Threading;
 
 namespace Algorithm.Services
 {
     public class DataReceiverService :  DataService.DataServiceBase
     {
-        /*public override async Task<AddOrderResponse> AddOrder(AddOrderRequest request, ServerCallContext context)
+        public override async Task<AddOrderResponse> AddOrder(IAsyncStreamReader<AddOrderRequest> requestStream, ServerCallContext context)
         {
-
-        }*/
+            while (await requestStream.MoveNext())
+            {
+                var order = requestStream.Current;
+                Console.WriteLine("Got something");
+            }
+            AddOrderResponse response = new AddOrderResponse();
+            return response;
+        }
     }
 }
-
-
-////посылается алгоритму
-//service DataService
-//{
-//	/*
-//		Перенаправляет стрим закрытых ордеров из ТМа к алгоритму
-//	*/
-//	rpc AddOrder (stream AddOrderRequest) returns ( AddOrderResponse);
-
-//}
-
-//message AddOrderRequest
-//{
-//	tradebot.common.v1.Order order = 1;
-//}
-////Есть ли смысл делать отдельный ответ если ничего не возвращается ?
-//message AddOrderResponse
-//{
-//	google.protobuf.Empty empty = 1;
-//}
 
