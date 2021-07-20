94a6464cfb082eb2c7427ad36cfb97a84c5e0e31
Elena <dezvencio@gmail.com>
2021-07-20 13:42:06 +0600

    надо переделать

---
+++ Algorithm/Analysis/AlgorithmAlpha.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Algorithm.Analysis
+{
+    //alpha takes all the points, calculates average with and without the last point
+    //if the trend had been descending for three poins and then started rising we're buying 
+    public class AlgorithmAlpha : IAlgorithm
+    {
+        public double CalculateSuggestedPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double CalculateSuggestedPrice(Dictionary<DateTime, double> points)
+        {
+
+        }
+    }
+}
--- Algorithm/DataManipulation/PointMaker.cs
+++ Algorithm/DataManipulation/PointMaker.cs
@@ -2,17 +2,37 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TradeBot.Common.v1;
 
 namespace Algorithm.DataManipulation
 {
     public class PointMaker : IPointRepository
     {
+        public bool initialAnalysisDone = false;
         public Dictionary<DateTime, double> GetPoints(string SlotID)
         {
             Dictionary<DateTime, double> requestedPoints = new Dictionary<DateTime, double>();
             return requestedPoints;
         }
 
+        public KeyValuePair<DateTime, double> MakePoint(List<Order> orders, DateTime timestamp)
+        {           
+            double price = 0;
 
+            foreach (Order order in orders)
+            {
+                price += order.Price;
+            }
+            price = price / orders.Count();
+
+            return new KeyValuePair<DateTime, double> (timestamp, price); 
+        }
+
+        public double CalculateEMA (Dictionary<DateTime, double> points)
+        {
+            SortedDictionary<DateTime, double> sortedPoints = new SortedDictionary<DateTime, double>(points);
+
+
+        }
     }
 }
