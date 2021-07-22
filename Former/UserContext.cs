180234e0664dcdfe2ae03ea0cfa7e92f771ff04e
Евгений Михайлюков <mihaylyukov99@gmail.com>
2021-07-22 12:55:53 +0600

    +

---
+++ Former/UserContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TradeBot.Common.v1;
+
+namespace Former
+{
+    public class UserContext
+    {
+        public Dictionary<string, Config> configures;
+    }
+}
