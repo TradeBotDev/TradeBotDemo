e7fb911c85ce1924a54baedb97ad6638a1fc1527
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-07-27 12:05:20 +0600

    Добавил тесты входа и регистрации, подправил поломанные старые.

---
+++ AccountTests/AccountServiceTests/LoginTests.cs
@@ -0,0 +1,53 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TradeBot.Account.AccountService.v1;
+using Xunit;
+
+namespace AccountTests.AccountServiceTests
+{
+    public class LoginTests
+    {
+        Random random = new Random();
+        AccountGRPC.AccountService service = new AccountGRPC.AccountService();
+
+        [Fact]
+        public void LoginToExistingAccount()
+        {
+            var registerRequest = new RegisterRequest
+            {
+                Email = $"existing_user{random.Next(0, 10000)}@pochta.test",
+                Password = "password",
+                VerifyPassword = "password"
+            };
+
+            var loginRequest = new LoginRequest()
+            {
+                Email = registerRequest.Email,
+                Password = "password",
+                SaveExchangesAfterLogout = false
+            };
+            var reply = service.Register(registerRequest, null);
+            reply.ContinueWith(login => service.Login(loginRequest, null));
+
+            Assert.Equal(ActionCode.Successful, reply.Result.Result);
+        }
+
+        [Fact]
+        public void LoginToNonExistingAccount()
+        {
+            var request = new LoginRequest()
+            {
+                Email = $"non_existing_user{random.Next(0, 10000)}@pochta.ru",
+                Password = "password",
+                SaveExchangesAfterLogout = false
+            };
+
+            var reply = service.Login(request, null);
+            Assert.Equal(ActionCode.AccountNotFound, reply.Result.Result);
+        }
+    }
+}
--- AccountTests/AccountServiceTests/RegisterTests.cs
+++ AccountTests/AccountServiceTests/RegisterTests.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TradeBot.Account.AccountService.v1;
 using Xunit;
 
@@ -10,14 +6,38 @@ namespace AccountTests.AccountServiceTests
 {
     public class RegisterTests
     {
+        Random random = new Random();
+        AccountGRPC.AccountService service = new AccountGRPC.AccountService();
+
+        // Тестирование на работу регистрации нового аккаунта.
+        [Fact]
         public void RegisterNewAccountTest()
         {
-            // Пока пусто
+            var request = new RegisterRequest
+            {
+                Email = $"user{random.Next(0, 10000)}@pochta.test",
+                Password = "password",
+                VerifyPassword = "password"
+            };
+            
+            var reply = service.Register(request, null);
+            Assert.Equal(ActionCode.Successful, reply.Result.Result);
         }
-
+        
+        // Тестирование на работу попытки регистрации существующего аккаунта.
+        [Fact]
         public void RegisterExistingAccount()
         {
-            // Пока пусто
+            var request = new RegisterRequest
+            {
+                Email = "user@pochta.test",
+                Password = "password",
+                VerifyPassword = "password"
+            };
+
+            service.Register(request, null);
+            var reply = service.Register(request, null);
+            Assert.Equal(ActionCode.AccountExists, reply.Result.Result);
         }
     }
 }
