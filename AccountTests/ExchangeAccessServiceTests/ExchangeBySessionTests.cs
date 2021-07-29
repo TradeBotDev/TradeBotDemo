19dde85488a3c042199f0c0ea29df7c53f047c2e
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-07-29 11:51:35 +0600

    Написал тест на чтение информации о доступе к бирже.

--- AccountTests/ExchangeAccessServiceTests/ExchangeBySessionTests.cs
+++ AccountTests/ExchangeAccessServiceTests/ExchangeBySessionTests.cs
@@ -50,10 +50,42 @@ namespace AccountTests.ExchangeAccessServiceTests
             Assert.Equal(ActionCode.AccountNotFound, reply.Result.Result);
         }
 
+        // Тестирование получения существующей информации о доступе к бирже из существующего аккаунта.
         [Fact]
         public void GetExistingExchangeAccess()
         {
-            // Пока пусто
+            // Очистка списка вошедших аккаунтов для того, чтобы не было конфликтов.
+            State.loggedIn = new();
+            string sessionId = "none";
+
+            // Локальный метод, генерирующий запрос на добавление информации о доступе к бирже
+            // и записывающий Id сессии в переменную.
+            AddExchangeAccessRequest GenerateRequest(string _sessionId)
+            {
+                sessionId = _sessionId;
+                return new AddExchangeAccessRequest
+                {
+                    Code = ExchangeCode.Bitmex,
+                    ExchangeName = "Bitmex",
+                    SessionId = sessionId,
+                    Token = "test_token",
+                    Secret = "test_secret"
+                };
+            }
+
+            // Последовательная генерация входа в аккаунт, добавление информации о доступе к биржи в аккаунт,
+            // а затем ее чтение.
+            var reply = GenerateLogin("one_existing_exchange")
+                .ContinueWith(loginReply => exchangeAccessService.AddExchangeAccess(GenerateRequest(loginReply.Result.Result.SessionId), null))
+                .ContinueWith(none => exchangeAccessService.ExchangeBySession(new ExchangeBySessionRequest
+                {
+                    SessionId = sessionId,
+                    Code = ExchangeCode.Bitmex
+                }, null));
+
+            // Ожидается, что получение информации будет завершено успешно и объект с информацией не будет пустым.
+            Assert.Equal(ActionCode.Successful, reply.Result.Result.Result);
+            Assert.NotNull(reply.Result.Result.Exchange);
         }
     }
 }
