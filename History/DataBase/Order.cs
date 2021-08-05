95cfde0e72bd51d1fee08bbf9615aba042a1f730
Elena <dezvencio@gmail.com>
2021-08-05 14:10:19 +0600

    added a new project

---
+++ History/DataBase/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace History.DataBase
+{
+    public class Order
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public Order Contract { get; set; }
+    }
+}
