47fdbc12d0f80b7f5b33f1ac5ec4f2ca5359b165
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-08-06 16:02:57 +0600

    Дописал метод

--- Website/Controllers/Clients/LicenseClient.cs
+++ Website/Controllers/Clients/LicenseClient.cs
@@ -1,8 +1,4 @@
 ﻿using Grpc.Net.Client;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TradeBot.License.LicenseService.v1;
 
 namespace Website.Controllers.Clients
@@ -11,21 +7,28 @@ namespace Website.Controllers.Clients
     {
         private static License.LicenseClient client = new(GrpcChannel.ForAddress("http://localhost:5007"));
 
-        public static SetLicenseResponse SetLicense(string sessionId, ProductCode productCode)
+        public static SetLicenseResponse SetLicense(string sessionId, ProductCode product)
         {
             int accountId = AccountServiceClient.AccountData(sessionId).CurrentAccount.AccountId;
             var request = new SetLicenseRequest
             {
                 AccountId = accountId,
-                Product = productCode
+                Product = product
             };
 
             return client.SetLicense(request);
         }
 
-        public static CheckLicenseResponse CheckLicense()
+        public static CheckLicenseResponse CheckLicense(string sessionId, ProductCode product)
         {
-            return null;
+            int accountId = AccountServiceClient.AccountData(sessionId).CurrentAccount.AccountId;
+            var request = new CheckLicenseRequest
+            {
+                AccountId = accountId,
+                Product = product
+            };
+
+            return client.CheckLicense(request);
         }
     }
 }
