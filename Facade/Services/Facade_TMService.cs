dea2f330a0746bf03cda466274679a33028a4b48
Dimty <nikitin-dmitriyy@mail.ru>
2021-08-12 17:15:06 +0300

    all work

--- Facade/Services/Facade_TMService.cs
+++ Facade/Services/Facade_TMService.cs
@@ -23,6 +23,7 @@ namespace Facade
         //public TradeBot.Account.AccountService.v1.Account.AccountClient ClientAccount { get => clientAccount; set => clientAccount = value; }
 
         private AutorisationClass clientAccount = new AutorisationClass();
+        private HistoryClass clientHistory = new HistoryClass();
         private RelayClass clientRelay = new RelayClass();
         private TradeMarketClass clientTM = new TradeMarketClass();
         #endregion
@@ -30,7 +31,7 @@ namespace Facade
         #region TradeMarket
         public override async Task SubscribeBalance(SubscribeBalanceRequest request, IServerStreamWriter<SubscribeBalanceResponse> responseStream, ServerCallContext context)
         {
-            await clientTM.TM_SubscribeBalance(request, responseStream, context,nameof(SubscribeBalance));
+            await clientTM.TM_SubscribeBalance(request, responseStream, context, nameof(SubscribeBalance));
         }
         public override async Task Slots(SlotsRequest request, IServerStreamWriter<SlotsResponse> responseStream, ServerCallContext context)
         {
@@ -42,71 +43,78 @@ namespace Facade
         }
         public async override Task<AuthenticateTokenResponse> AuthenticateToken(AuthenticateTokenRequest request, ServerCallContext context)
         {
-            return await clientTM.TM_AuthenticateToken(request,nameof(AuthenticateToken));
+            return await clientTM.TM_AuthenticateToken(request, nameof(AuthenticateToken));
         }
         #endregion
 
         #region Relay
         public override async Task SubscribeLogsRelay(SubscribeLogsRequest request, IServerStreamWriter<SubscribeLogsResponse> responseStream, ServerCallContext context)
         {
-            await clientRelay.Relay_SubscribeLogs(request, responseStream, context,nameof(SubscribeLogsRelay));
+            await clientRelay.Relay_SubscribeLogs(request, responseStream, context, nameof(SubscribeLogsRelay));
         }
         public async override Task<DeleteOrderResponse> DeleteOrder(DeleteOrderRequest request, ServerCallContext context)
         {
-            return await clientRelay.Relay_DeleteOrder(context,nameof(DeleteOrder));
+            return await clientRelay.Relay_DeleteOrder(context, nameof(DeleteOrder));
         }
         public async override Task<SwitchBotResponse> StartBot(SwitchBotRequest request, ServerCallContext context)
         {
-            return await clientRelay.Relay_StartBot(request, context,nameof(StartBot));
+            return await clientRelay.Relay_StartBot(request, context, nameof(StartBot));
 
         }
         public async override Task<StopBotResponse> StopBot(StopBotRequest request, ServerCallContext context)
         {
-            return await clientRelay.Relay_stopBot(request, context,nameof(StopBot));
+            return await clientRelay.Relay_stopBot(request, context, nameof(StopBot));
         }
         public async override Task<UpdateServerConfigResponse> UpdateServerConfig(UpdateServerConfigRequest request, ServerCallContext context)
         {
-            return await clientRelay.Relay_UpdateServerConfig(request, context,nameof(UpdateServerConfig));
+            return await clientRelay.Relay_UpdateServerConfig(request, context, nameof(UpdateServerConfig));
         }
         #endregion
 
         #region Account
         public async override Task<LoginReply> Login(LoginRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_Login(request,nameof(Login));
+            return await clientAccount.Account_Login(request, nameof(Login));
         }
         public async override Task<LogoutReply> Logout(SessionRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_Logout(request, context,nameof(Logout));
+            return await clientAccount.Account_Logout(request, context, nameof(Logout));
         }
         public async override Task<RegisterReply> Register(RegisterRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_Register(request, context,nameof(Register));
+            return await clientAccount.Account_Register(request, context, nameof(Register));
         }
         public async override Task<CurrentAccountReply> CurrentAccountData(SessionRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_CurrentAccountData(request, context,nameof(CurrentAccountData));
+            return await clientAccount.Account_CurrentAccountData(request, context, nameof(CurrentAccountData));
 
         }
         public async override Task<AddExchangeAccessReply> AddExchangeAccess(AddExchangeAccessRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_AddExcengeAccess(request, context,nameof(AddExchangeAccess));
+            return await clientAccount.Account_AddExcengeAccess(request, context, nameof(AddExchangeAccess));
 
         }
         public async override Task<AllExchangesBySessionReply> AllExchangesBySession(SessionRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_AllExcangesBySession(request, context,nameof(AllExchangesBySession));
+            return await clientAccount.Account_AllExcangesBySession(request, context, nameof(AllExchangesBySession));
         }
         public async override Task<DeleteExchangeAccessReply> DeleteExchangeAccess(DeleteExchangeAccessRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_DeleteChangesAccess(request, context,nameof(DeleteExchangeAccess));
+            return await clientAccount.Account_DeleteChangesAccess(request, context, nameof(DeleteExchangeAccess));
 
         }
         public async override Task<ExchangeBySessionReply> ExchangeBySession(ExchangeBySessionRequest request, ServerCallContext context)
         {
-            return await clientAccount.Account_ExchangeBySession(request, context,nameof(ExchangeBySession));
+            return await clientAccount.Account_ExchangeBySession(request, context, nameof(ExchangeBySession));
         }
         #endregion
 
+        #region History
+        public async override Task SubscribeEvents(SubscribeEventsRequest request, IServerStreamWriter<SubscribeEventsResponse> responseStream, ServerCallContext context)
+        {
+            await clientHistory.History_SubscribeEvents(request, responseStream, context);
+        }
+
+        #endregion
     }
 }
