76aa5ae2c3c5061b00ab1b21f22b437892672900
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-08-13 16:14:10 +0600

    Добавил еще тест

--- AccountTests/LicenseServiceTests/CheckLicenseTests.cs
+++ AccountTests/LicenseServiceTests/CheckLicenseTests.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TradeBot.Account.AccountService.v1;
+﻿using TradeBot.Account.AccountService.v1;
 using Xunit;
 
 namespace AccountTests.LicenseServiceTests
@@ -34,6 +29,34 @@ namespace AccountTests.LicenseServiceTests
                 }, null));
 
             Assert.Equal(LicenseCode.NoAccess, reply.Result.Result.Code);
+        }
+
+        [Fact]
+        public void CheckExistingLicense()
+        {
+            string session_id = null;
+            string WriteSessionId(string _session_id)
+            {
+                session_id = _session_id;
+                return _session_id;
+            }
+
+            var reply = GenerateLogin("ex_license").ContinueWith(login =>
+               licenseService.SetLicense(new SetLicenseRequest
+               {
+                   SessionId = WriteSessionId(login.Result.Result.SessionId),
+                   Product = ProductCode.Tradebot,
+                   CardNumber = "1234567812345678",
+                   Date = 1234,
+                   Cvv = 123
+               }, null)).ContinueWith(set =>
+                    licenseService.CheckLicense(new CheckLicenseRequest
+                    {
+                        SessionId = session_id,
+                        Product = ProductCode.Tradebot
+                    }, null));
+
+            Assert.Equal(LicenseCode.HaveAccess, reply.Result.Result.Code);
         }
     }
 }
