a0b68e54761082058290216b591576cd11b71d6a
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-08-13 16:47:37 +0600

    Добавил комментариев

--- AccountTests/LicenseServiceTests/SetLicenseTests.cs
+++ AccountTests/LicenseServiceTests/SetLicenseTests.cs
@@ -1,9 +1,4 @@
 ﻿using TradeBot.Account.AccountService.v1;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace AccountTests.LicenseServiceTests
@@ -11,9 +6,11 @@ namespace AccountTests.LicenseServiceTests
     [Collection("AccountTests")]
     public class SetLicenseTests : LicenseServiceTestsData
     {
+        // Проверка добавления лицензии в несуществующий аккаунт.
         [Fact]
         public void SetLicenseToNotExistingAccount()
         {
+            // Запрос на установку лицензии.
             var reply = licenseService.SetLicense(new SetLicenseRequest
             {
                 SessionId = "not_existing_session_id",
@@ -22,13 +19,15 @@ namespace AccountTests.LicenseServiceTests
                 Date = 1234,
                 Cvv = 123
             }, null);
-
+            // Ожидается, что пользователь не будет иметь доступа к лицензии, потому что
+            // аккаунт не будет существовать.
             Assert.Equal(LicenseCode.NoAccess, reply.Result.Code);
         }
 
         [Fact]
         public void SetLicenseToExistingAccount()
         {
+            // Генерация аккаунта и добавление в него лицензии.
             var reply = GenerateLogin("set_lic_to_ex").ContinueWith(login =>
                 licenseService.SetLicense(new SetLicenseRequest
                 {
@@ -38,7 +37,7 @@ namespace AccountTests.LicenseServiceTests
                     Date = 1234,
                     Cvv = 123
                 }, null));
-
+            // Ожидается, что добавление лицензии будет успешным.
             Assert.Equal(LicenseCode.Successful, reply.Result.Result.Code);
         }
     }
