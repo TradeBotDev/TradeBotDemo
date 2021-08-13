f55530884f00355ff757f946aa576422e0dcbcdc
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-08-13 15:09:30 +0600

    Добавил комментариев в HomeController и PurchaseController

--- Website/Controllers/HomeController.cs
+++ Website/Controllers/HomeController.cs
@@ -6,8 +6,10 @@ namespace Website.Controllers
 {
     public class HomeController : Controller
     {
+        // Метод, показывающий главную страницу сайта.
         public async Task<IActionResult> Index()
         {
+            // Проверка лицензии и передача ее результата в представление через ViewBag.
             var haveLicense = await Clients.LicenseClient.CheckLicense(User.Identity.Name, ProductCode.Tradebot);
             ViewBag.HaveLicense = haveLicense.HaveAccess;
             return View();
--- Website/Controllers/PurchaseController.cs
+++ Website/Controllers/PurchaseController.cs
@@ -6,12 +6,15 @@ using Website.Models;
 
 namespace Website.Controllers
 {
+    // Все страницы в данном контроллере требуют авторизации для доступа к ним.
     [Authorize]
     public class PurchaseController : Controller
     {
+        // Метод, показывающий страницу покупки приложения.
         [HttpGet]
         public async Task<IActionResult> Buy()
         {
+            // Проверка лицензии и передача ее результата в представление через ViewBag.
             var haveLicense = await Clients.LicenseClient.CheckLicense(User.Identity.Name, ProductCode.Tradebot);
             ViewBag.HaveLicense = haveLicense.HaveAccess;
             return View();
@@ -19,15 +22,21 @@ namespace Website.Controllers
 
         public async Task<IActionResult> Buy(CreditCardModel model)
         {
+            // Проверка лицензии и передача ее результата в представление через ViewBag.
             var haveLicense = await Clients.LicenseClient.CheckLicense(User.Identity.Name, ProductCode.Tradebot);
             ViewBag.HaveLicense = haveLicense.HaveAccess;
 
-            if (!ModelState.IsValid || haveLicense.HaveAccess)
+            // Если данные модели являются невалидными, возвращается та же страница с сообщениями об ошибке.
+            if (!ModelState.IsValid)
                 return View();
 
+            // Отправка запроса на получение лицензии.
             var reply = await Clients.LicenseClient.SetLicense(User.Identity.Name, ProductCode.Tradebot, model);
+
+            // Если лицензия была получена успешно, происходит перенаправление на страницу аккаунта.
             if (reply.Code == LicenseCode.Successful)
                 return RedirectToAction("Account", "Account");
+            // Иначе возвращается страница с сообщением об ошибке.
             else return View("~/Views/Shared/Error.cshtml", reply.Message);
         }
     }
