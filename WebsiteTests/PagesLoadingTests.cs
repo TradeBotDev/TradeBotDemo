98b27b0443a4a112e0e7c0bcf9da1d8a028ff40d
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-08-13 17:26:48 +0600

    Добавил тесты адресов, но они все падают из-за того, что не запущен AccountService (ошибка 500)

---
+++ WebsiteTests/PagesLoadingTests.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebsiteTests
+{
+    public class PagesLoadingTests : IClassFixture<WebApplicationFactory<Website.Startup>>
+    {
+        private readonly WebApplicationFactory<Website.Startup> _factory;
+
+        public PagesLoadingTests(WebApplicationFactory<Website.Startup> factory) => _factory = factory;
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Account")]
+        [InlineData("/Account/AddExchangeAccess")]
+        [InlineData("/Authorization/Login")]
+        [InlineData("/Authorization/Register")]
+        [InlineData("/Authorization/Logout")]
+        [InlineData("/Home/Index")]
+        [InlineData("/Purchase/Buy")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            Assert.Equal("text/html; charset=utf-8", response.Content.Headers.ContentType.ToString());
+        }
+    }
+}
