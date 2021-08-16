36b5c61d84070071e82c820a6a0030014e2382ae
Elena <dezvencio@gmail.com>
2021-08-16 10:38:10 +0600

    +

---
+++ History/DataBase/Data Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using TradeBot.Common.v1;
+
+namespace TradeBot.Common.v1
+{
+    public partial class Order
+    {
+        [Key]
+        int DBId { get; set; }
+    }
+}
