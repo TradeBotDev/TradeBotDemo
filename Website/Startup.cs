6ca7cb83acd4279f2fb3119fe47088691597dda5
DESKTOP-L9JNC77\lisowolf <minebuilder445@gmail.com>
2021-08-19 00:08:16 +0600

    Пытался сделать авторизацию с Blazor, но не вышло. Компонент не используется

--- Website/Startup.cs
+++ Website/Startup.cs
@@ -35,6 +35,7 @@ namespace Website
 			});
 			services.AddControllersWithViews();
 			services.AddServerSideBlazor();
+			services.AddHttpContextAccessor();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
