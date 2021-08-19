364088b951110dbea180a98bfcac0aed54dcab8f
Unh0ly666 <mihaylyukov00@mail.ru>
2021-08-19 11:19:37 +0600

    +

---
+++ Former/Clients/RedisClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Grpc.Core;
+using StackExchange.Redis;
+using Newtonsoft.Json;
+
+namespace Former.Clients
+{
+    public class RedisClient
+    {
+        private readonly IDatabase _database;
+        private const string ServiceId = "5003";
+
+        public RedisClient()
+        {
+            var redis = ConnectionMultiplexer.Connect(new ConfigurationOptions{ EndPoints = {Environment.GetEnvironmentVariable("REDIS_CONNECTION_STRING")} });
+            _database = redis.GetDatabase();
+        }
+
+        public void WriteContext(List<Metadata> contexts)
+        {
+            var json = 
+
+
+        }
+
+        public bool ReadContext()
+        {
+
+        }
+
+
+    }
+}
