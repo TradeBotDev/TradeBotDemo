73d8a81be75863e017c6ec864e29aecf0af0bb98
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-08-20 17:39:50 +0600

    Переезд на PostgreSQL

--- Account/Models/AccountContext.cs
+++ Account/Models/AccountContext.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
+// Подключение к СУБД в Docker:
+// docker run -p 5432:5432 -d -e POSTGRES_PASSWORD=postgres -e POSTGRES_USER=postgres -e POSTGRES_DB=postgresdb postgres
+
 namespace AccountGRPC.Models
 {
     public class AccountContext : DbContext
@@ -17,14 +20,15 @@ namespace AccountGRPC.Models
                 .Build();
 
             // Получение строки подкючения из appsettings.json.
-            connectionString = configuration.GetConnectionString("AccountConnection");
+            connectionString = configuration.GetConnectionString("PostgreSQL");
             // Создание базы данных, если она еще не была создана.
             Database.EnsureCreated();
         }
 
-        // Указание, что будет использовать SQLite и файл из строки подключения для него (accounts.db).
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite(connectionString);
-        
+        // Указание, что будет использовать PostgreSQL и файл из строки подключения для него.
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql(connectionString);
+
         // Таблица с данными аккаунтов.
         public DbSet<Account> Accounts { get; set; }
 
