d501797d0227196c0ed595462b48be2c8505c03a
Unh0ly666 <mihaylyukov00@mail.ru>
2021-08-24 15:55:07 +0600

    исправил отписку истории

--- TradeMarket/DataTransfering/Bitmex/Publishers/InstrumentPublisher.cs
+++ TradeMarket/DataTransfering/Bitmex/Publishers/InstrumentPublisher.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Serilog;
 using TradeMarket.Model.Publishers;
 
 namespace TradeMarket.DataTransfering.Bitmex.Publishers
