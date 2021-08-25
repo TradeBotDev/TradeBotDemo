8e75f9895eccc22c2fe092f35a84c32ea833144b
DESKTOP-JVI15QG\super <minebuilder445@gmail.com>
2021-08-25 11:11:25 +0600

    Добавил placeholder для полей ввода

--- Website/Models/CreditCardModel.cs
+++ Website/Models/CreditCardModel.cs
@@ -5,7 +5,7 @@ namespace Website.Models
 	public class CreditCardModel
 	{
 		[Required(ErrorMessage = "Ввод номера карты является обязательным.")]
-		[StringLength(16, MinimumLength = 16, ErrorMessage = "Длина номера карты должна быть равна 16 символам.")]
+		[StringLength(19, MinimumLength = 19, ErrorMessage = "Длина номера карты должна содержать в себе 16 чисел.")]
 		public string CardNumber { get; set; }
 
 		[Required(ErrorMessage = "Отсутствуют данные в поле ввода даты.")]
